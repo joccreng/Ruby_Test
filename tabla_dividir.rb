88ea15a4f660f37e7f1b9e0d0ea4e8e694220b21
joccreng <joc.rengifoo@gmail.com>
2021-04-27 16:22:18 -0400

    otra prueba

--- tabla_dividir.rb
+++ tabla_dividir.rb
@@ -1,22 +1,9 @@
 def tabla(div)
-	div = numero * n
+	print "Coloca un numero: "
+	numero = gets.chomp.to_i
+	 (1..10).each do |n|
+	 	div = numero * n
+		puts " #{div} entre #{numero} = #{n}}"
+	end
 end
-print "Coloca un numero: "
-numero = gets.chomp.to_i
- (1..10).each do |n|
-	puts " #{div} entre #{numero} = #{n}}"
-end
-
-
-
-
-
-
-
-#print "Coloca un numero :"
-#numero = gets.chomp.to_i
-
-#(1..10).each do |n|
-#	div = numero * n 
-#	puts "#{div} entre #{numero} = #{n}"
-#end
+puts tabla
