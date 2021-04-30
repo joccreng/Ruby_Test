985338558807d800742fe399d0c1e024c9a7bc76
joccreng <joc.rengifoo@gmail.com>
2021-04-30 16:24:34 -0400

    ahora si ta listo xd

--- tabla_dividir.rb
+++ tabla_dividir.rb
@@ -1,11 +1,11 @@
+print "Coloque el numero para su tabla : "
+a = gets.chomp.to_i
 
-def tabla(div)
-	print "Coloca un numero : "
-	a = gets.chomp.to_i
+def tabla(a)
 	 (1..10).each do |n|
 	 	div = a * n
 		puts " #{div} entre #{a} = #{n}"
 	end
 end
-a = a
+
 tabla(a)
