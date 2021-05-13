35618a5ddddfbcaafb8bc6a0cac1212bc69d9384
joccreng <joc.rengifoo@gmail.com>
2021-05-12 21:07:01 -0400

    wip: tarea del miercoles

--- miercoles.rb
+++ miercoles.rb
@@ -1,17 +1,24 @@
+
+likes = to_s
+
+
 def facebook(likes)
     case likes
-	when 1000
+    when 0..999
+    	puts "#{likes}"
+	when 1000..9000
 		puts "1k"
-	when 10000
+	when 10000..90000
 		puts "10k"
-	when 999999
+	when 100000..999999
 		puts "999k"
-	 when 1000000
+	 when 1000000..999000000
  		puts "1M"
  	end
 end
 
+facebook(422)
 facebook(1000)
-facebook(10000)
+facebook(30000)
 facebook(999999)
-facebook(1000000)
+facebook(5000000)
