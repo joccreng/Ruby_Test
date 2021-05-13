7754b1c4ffbd61b8d6dcfa99037d49b8e1ad2768
joccreng <joc.rengifoo@gmail.com>
2021-05-12 21:53:03 -0400

    wip: Tarea del miercoles

--- miercoles.rb
+++ miercoles.rb
@@ -5,20 +5,20 @@ likes = to_s
 def facebook(likes)
     case likes
     when 0..999
-    	puts "#{likes}"
-	when 1000..9000
-		puts "1k"
-	when 10000..90000
-		puts "10k"
-	when 100000..999999
-		puts "999k"
-	 when 1000000..999000000
- 		puts "1M"
+    	puts "#{likes}k"
+	when 1.000..9.000
+		puts "#{likes}K"
+	when 10.000..90.000
+		puts "#{likes}K" 
+	when 100.000..999.999
+		puts "#{likes}K"
+	 when 1_000_000..999_000_000
+ 		puts "#{likes.to_i}M"
  	end
 end
 
 facebook(422)
-facebook(1000)
-facebook(30000)
-facebook(999999)
-facebook(5000000)
+facebook(9.000.to_i)
+facebook(20.000.to_i)
+facebook(340.999.to_i)
+facebook(5_000_000.to_i)
